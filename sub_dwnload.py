6510a6b450e9e24af61864a43799a7a074af5a68
fedelico <u105000017@cmu.edu.tw>
2021-01-05 22:58:58 +0800

    Remove todo list

--- sub_dwnload.py
+++ sub_dwnload.py
@@ -1,13 +1,3 @@
-"""
-Todo list:
-    - features
-        -scan through target directories(default to cwd if not specified) and find those mp3 files which haven't had their subtitle files
-        -search through source webpages by name of the song and artist(s)(currently vvlyrics.com)
-            -find out ways to get name of the song and artist
-        -download subtitle files to the target directories
-        -program exits if everthing works properly, otherwise show relevant readable error messages
-
-"""
 import sys
 import os
 import requests
